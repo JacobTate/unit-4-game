5e71465d3f6d9e886f97d912c83484869925df93
Jacob Tate <jacob87608@gmail.com>
2020-01-07 12:43:39 -0600

    added images added a rng for 1-100 and added rng 1-10 to the images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,44 @@
 $(document).ready(function () {
 
+    //variables
 
 
-    
+    var img1_num = Math.floor((Math.random() * 10) + 1);
+    var img2_num = Math.floor((Math.random() * 10) + 1);
+    var img3_num = Math.floor((Math.random() * 10) + 1);
+    var img4_num = Math.floor((Math.random() * 10) + 1);
+    //gets a random number 1-100
+    var random_number = Math.floor((Math.random() * 100) + 1);
+    //displays the number
+
+    $("#game_num").text(random_number);
+
+    $(".image").on("click", function () {
+
+
+
+
+
+        var picked_image = $(this).val();
+
+        console.log(picked_image);
+        //if the image clicked is equal to the value of the image (button)
+        if (picked_image === "1") {
+            console.log(img1_num);
+        } else if (picked_image === "2") {
+            console.log(img2_num);
+        } else if (picked_image === "3") {
+            console.log(img3_num);
+        } else if (picked_image === "4") {
+            console.log(img4_num);
+        }
+
+
+
+
+    });
+
+
+
+
 });
